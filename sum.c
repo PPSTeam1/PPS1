5afc54bd96f4fa450767846e1e56781c5cd95fe5
PPSTeam1 <101404854+PPSTeam1@users.noreply.github.com>
2022-03-21 11:39:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   int num1,num2;
+    printf("Enter a number : ");
+    scanf("%d",&num1);
+
+    printf("Enter a number : ");
+    scanf("%d",&num2);
+
+    printf("Sum of %d and %d is %d",num1,num2,num1+num2);
+    return 0;
+}
