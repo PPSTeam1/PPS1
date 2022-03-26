bef3716d2d2e357339a3b5f20171f8cfd8e6692c
Narender Singh Group <101404854+PPSTeam1@users.noreply.github.com>
2022-03-26 23:02:01 +0530

    Add files via upload

---
+++ library-fine.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+    int days;
+    float fine;
+
+    printf("enter the number of late days : ");
+    scanf("%d",&days);
+
+    if(days<=30)
+    {
+        if(days<=30&&days>10)
+            fine=7.5+((days-10)*5);
+        else if(days<=10&&days>5)
+            fine=2.5+((days-5)*1);
+        else if(days<6&&days>0)
+            fine=days*0.5;
+        printf("fine =  %.2f\n\n",fine);
+
+    }
+    else printf("your membership is cancelled\n\n");
+
+}
