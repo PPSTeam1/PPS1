d3ae0e3db5316c12b40178a9d12491d257637e1b
Narender Singh Kapany <101404854+PPSTeam1@users.noreply.github.com>
2022-06-02 01:18:28 +0530

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 17,b = 27;
+
+
+    printf("Integer value of a = %d and b = %d \n",a,b);
+    printf("Floating value of a = %f and b = %f",(float)(a),(float)(b));
+
+    return 0;
+}
